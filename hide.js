05ea35567162859707c9d7c917cc3b87f43fe6e1
Abiodun Agbolade <biodun.agbolade@gmail.com>
2019-09-11 23:02:59 +0100

    hide and seek

---
+++ hide.js
@@ -0,0 +1,19 @@
+var toggleStatus = 1
+
+function toggleMenu(){
+    if (toggleStatus == 1){
+        document.getElementById("menu").style.left = "-240px";
+        toggleStatus = 0;
+        }else if (toggleStatus == 0) {
+            document.getElementById("menu").style.left = "0px";
+            toggleStatus = 1;
+        }
+}
+
+var myHead = document.getElementById('toggleid')
+
+function toggleid () {
+        myHead.style.color = "red";
+
+}
+
